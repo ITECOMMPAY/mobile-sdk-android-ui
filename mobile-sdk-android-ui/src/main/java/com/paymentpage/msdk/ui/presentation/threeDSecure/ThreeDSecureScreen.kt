ae442108d506fd43b9006fbf203cb0b6afcd849e
Alexey Khrameev <a.khrameev@it.ecommpay.com>
2022-07-27 21:18:44 +0300

    changed webview loading for acs page added simple handling of cascading

--- mobile-sdk-android-ui/src/main/java/com/paymentpage/msdk/ui/presentation/threeDSecure/ThreeDSecureScreen.kt
+++ mobile-sdk-android-ui/src/main/java/com/paymentpage/msdk/ui/presentation/threeDSecure/ThreeDSecureScreen.kt
@@ -1,9 +1,11 @@
 package com.paymentpage.msdk.ui.presentation.threeDSecure
 
+import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.net.http.SslError
 import android.view.ViewGroup
 import android.webkit.SslErrorHandler
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.compose.BackHandler
@@ -16,13 +18,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import com.paymentpage.msdk.core.domain.entities.threeDSecure.AcsPage
 import com.paymentpage.msdk.ui.LocalMainViewModel
 import com.paymentpage.msdk.ui.PaymentActivity
+import com.paymentpage.msdk.ui.R
 import com.paymentpage.msdk.ui.presentation.main.threeDSecureHandled
+import com.paymentpage.msdk.ui.presentation.main.views.detail.PaymentDetailsView
 import com.paymentpage.msdk.ui.theme.SDKTheme
 import com.paymentpage.msdk.ui.views.common.CustomButton
+import com.paymentpage.msdk.ui.views.common.PaymentOverview
+import com.paymentpage.msdk.ui.views.common.SDKFooter
 import com.paymentpage.msdk.ui.views.common.SDKScaffold
 
 @Composable
@@ -31,94 +39,129 @@ internal fun ThreeDSecureScreen(
 ) {
     val viewModel = LocalMainViewModel.current
     val acsPage = viewModel.lastState.acsPageState?.acsPage
-    var isLoading by remember { mutableStateOf(false) }
+    val isCascading = remember { viewModel.lastState.acsPageState?.isCascading ?: false }
+
+    var isCascadingShowing by remember { mutableStateOf(isCascading) }
+
     BackHandler(true) { onCancel() }
 
     SDKScaffold(
         notScrollableContent = {
-            if (isLoading)
-                Box(modifier = Modifier
-                    .height(LocalConfiguration.current.screenHeightDp.dp * 0.9f) //Height of bottom sheet
-                    .fillMaxWidth()
-                    .background(SDKTheme.colors.backgroundColor),
-                    contentAlignment = Alignment.Center
-                ) {
-                    CircularProgressIndicator(
-                        color = SDKTheme.colors.brand
-                    )
-                }
-            if (PaymentActivity.isMockModeEnabled)
+            if (isCascadingShowing) {
+                PaymentDetailsView()
+                Spacer(modifier = Modifier.size(15.dp))
+            } else if (acsPage != null) {
+                AcsPageView(acsPage = acsPage)
+            }
+        },
+        scrollableContent = {
+            if (isCascadingShowing) {
+                PaymentOverview()
+                Spacer(modifier = Modifier.size(15.dp))
+                Text(
+                    modifier = Modifier.padding(10.dp),
+                    text = PaymentActivity.stringResourceManager.getStringByKey("cascade_error"),
+                    color = SDKTheme.colors.errorTextColor
+                )
+                Spacer(modifier = Modifier.size(15.dp))
                 CustomButton(
-                    modifier = Modifier.height(45.dp),
+                    modifier = Modifier,
                     isEnabled = true,
                     content = {
                         Text(
-                            text = "Page Handled",
+                            text = PaymentActivity.stringResourceManager.getStringByKey("cascade_payment_button"),
                             style = SDKTheme.typography.s16Normal.copy(color = Color.White)
                         )
-
                     },
                     onClick = {
-                        viewModel.threeDSecureHandled()
+                        isCascadingShowing = false
                     }
                 )
-            Column(modifier = Modifier.fillMaxSize()) {
-                AndroidView(modifier = Modifier
-                    .weight(1f)
-                    .background(SDKTheme.colors.backgroundColor),
-                    factory = {
-                        WebView(it).apply {
-                            layoutParams = ViewGroup.LayoutParams(
-                                ViewGroup.LayoutParams.MATCH_PARENT,
-                                ViewGroup.LayoutParams.MATCH_PARENT
-                            )
-                            webViewClient = object : WebViewClient() {
-                                override fun onPageStarted(
-                                    view: WebView?,
-                                    url: String?,
-                                    favicon: Bitmap?,
-                                ) {
-                                    super.onPageStarted(view, url, favicon)
-                                    isLoading = true
-                                    if (url.equals(acsPage?.acs?.termUrl)) {
-                                        viewModel.threeDSecureHandled()
-                                    }
-                                }
-
-                                override fun onPageFinished(view: WebView?, url: String?) {
-                                    super.onPageFinished(view, url)
-                                    if (!url.equals(acsPage?.acs?.acsUrl)) {
-                                        isLoading = false
-                                    }
-                                }
-
-                                override fun onReceivedSslError(
-                                    view: WebView?,
-                                    handler: SslErrorHandler?,
-                                    error: SslError?,
-                                ) {
-                                    super.onReceivedSslError(view, handler, error)
-
-                                }
-                            }
-                            settings.javaScriptEnabled = true
-                            acsPage?.let { page ->
-                                loadDataWithBaseURL(
-                                    page.acs?.acsUrl ?: "",
-                                    page.content ?: "",
-                                    "text/html",
-                                    "UTF-8",
-                                    null
-                                )
-                            }
-                        }
-                    }, update = {
-
-                    })
             }
         },
-        scrollableContent = {},
-        footerContent = { },
+        footerContent = {
+            if (isCascadingShowing)
+                SDKFooter(
+                    iconLogo = SDKTheme.images.sdkLogoResId,
+                    poweredByText = stringResource(R.string.powered_by_label),
+                )
+        },
         onClose = { onCancel() }
     )
+}
+
+@SuppressLint("SetJavaScriptEnabled")
+@Composable
+internal fun AcsPageView(
+    acsPage: AcsPage,
+) {
+    val viewModel = LocalMainViewModel.current
+    var isLoading by remember { mutableStateOf(false) }
+    if (isLoading)
+        Box(
+            modifier = Modifier
+                .height(LocalConfiguration.current.screenHeightDp.dp * 0.9f) //Height of bottom sheet
+                .fillMaxWidth()
+                .background(SDKTheme.colors.backgroundColor),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator(
+                color = SDKTheme.colors.brand
+            )
+        }
+    Column(modifier = Modifier.fillMaxSize()) {
+        AndroidView(modifier = Modifier
+            .weight(1f)
+            .background(SDKTheme.colors.backgroundColor),
+            factory = {
+                WebView(it).apply {
+                    layoutParams = ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                    )
+                    webChromeClient = object : WebChromeClient() {
+                        override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                            isLoading = newProgress < 100
+                        }
+                    }
+                    webViewClient = object : WebViewClient() {
+                        override fun onPageStarted(
+                            view: WebView?,
+                            url: String?,
+                            favicon: Bitmap?,
+                        ) {
+                            super.onPageStarted(view, url, favicon)
+                            isLoading = true
+                            if (url.equals(acsPage.acs?.termUrl)) {
+                                viewModel.threeDSecureHandled()
+                            }
+                        }
+
+                        override fun onPageFinished(view: WebView?, url: String?) {
+                            super.onPageFinished(view, url)
+                            isLoading = false
+                        }
+
+                        override fun onReceivedSslError(
+                            view: WebView?,
+                            handler: SslErrorHandler?,
+                            error: SslError?,
+                        ) {
+                            super.onReceivedSslError(view, handler, error)
+
+                        }
+                    }
+                    settings.javaScriptEnabled = true
+                    loadDataWithBaseURL(
+                        acsPage.acs?.acsUrl ?: "",
+                        acsPage.content ?: "",
+                        "text/html",
+                        "UTF-8",
+                        null
+                    )
+                }
+            }, update = {
+
+            })
+    }
 }
