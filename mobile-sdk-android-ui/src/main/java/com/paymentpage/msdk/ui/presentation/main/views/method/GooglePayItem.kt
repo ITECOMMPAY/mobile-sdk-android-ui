3dbae208734f436c5380a798e60867ef71b26f82
Roman Karpilenko <r.karpilenko@it.ecommpay.com>
2022-08-02 13:53:15 +0300

    MSDK-314: Added force logic to google pay

--- mobile-sdk-android-ui/src/main/java/com/paymentpage/msdk/ui/presentation/main/views/method/GooglePayItem.kt
+++ mobile-sdk-android-ui/src/main/java/com/paymentpage/msdk/ui/presentation/main/views/method/GooglePayItem.kt
@@ -1,20 +1,32 @@
 package com.paymentpage.msdk.ui.presentation.main.views.method
 
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.google.android.gms.wallet.IsReadyToPayRequest
+import com.google.android.gms.wallet.Wallet
+import com.google.android.gms.wallet.WalletConstants
 import com.paymentpage.msdk.core.base.ErrorCode
+import com.paymentpage.msdk.core.domain.entities.init.PaymentMethodType
+import com.paymentpage.msdk.core.domain.interactors.pay.googlePay.GooglePayEnvironment
+import com.paymentpage.msdk.core.googlePay.GooglePayHelper
 import com.paymentpage.msdk.ui.LocalMainViewModel
 import com.paymentpage.msdk.ui.LocalPaymentOptions
+import com.paymentpage.msdk.ui.PaymentActivity
 import com.paymentpage.msdk.ui.base.ErrorResult
 import com.paymentpage.msdk.ui.googlePay.GooglePayActivityContract
 import com.paymentpage.msdk.ui.presentation.main.models.UIPaymentMethod
 import com.paymentpage.msdk.ui.presentation.main.saleGooglePay
 import com.paymentpage.msdk.ui.presentation.main.views.method.expandable.ExpandablePaymentMethodItem
 import com.paymentpage.msdk.ui.theme.SDKTheme
+import com.paymentpage.msdk.ui.utils.extensions.core.hasVisibleCustomerFields
+import com.paymentpage.msdk.ui.utils.extensions.core.isAllCustomerFieldsHidden
+import com.paymentpage.msdk.ui.utils.extensions.core.visibleCustomerFields
 import com.paymentpage.msdk.ui.views.button.GooglePayButton
 import com.paymentpage.msdk.ui.views.customerFields.CustomerFields
 
@@ -22,17 +34,31 @@ import com.paymentpage.msdk.ui.views.customerFields.CustomerFields
 @Composable
 internal fun GooglePayItem(
     method: UIPaymentMethod.UIGooglePayPaymentMethod,
-    onError: (ErrorResult, Boolean) -> Unit
+    onError: (ErrorResult, Boolean) -> Unit,
 ) {
     val paymentOptions = LocalPaymentOptions.current
     val viewModel = LocalMainViewModel.current
     val customerFields = remember { method.paymentMethod.customerFields }
     val additionalFields = LocalPaymentOptions.current.additionalFields
-    val visibleCustomerFields = remember { customerFields.filter { !it.isHidden } }
-    val merchantId = LocalPaymentOptions.current.merchantId
-
+    val visibleCustomerFields = remember { customerFields.visibleCustomerFields() }
     var isCustomerFieldsValid by remember { mutableStateOf(method.isCustomerFieldsValid) }
+    val isForcePaymentMethod = paymentOptions.paymentInfo.forcePaymentMethod == PaymentMethodType.GOOGLE_PAY
 
+    val merchantId = LocalPaymentOptions.current.merchantId
+    val merchantName = LocalPaymentOptions.current.merchantName
+    var isGooglePayAvailable by remember { mutableStateOf(isForcePaymentMethod) }
+    var isGooglePayOpened by remember { mutableStateOf(false) }
+    val googlePayHelper = GooglePayHelper(merchantId, merchantName)
+    val activity = LocalContext.current as PaymentActivity
+    val googlePayClient = remember {
+        Wallet.getPaymentsClient(
+            activity,
+            Wallet.WalletOptions.Builder()
+                .setEnvironment(if (paymentOptions.merchantEnvironment == GooglePayEnvironment.TEST) WalletConstants.ENVIRONMENT_TEST else WalletConstants.ENVIRONMENT_PRODUCTION)//test
+                //.setTheme(WalletConstants.THEME_LIGHT)
+                .build()
+        )
+    }
     val handle: (GooglePayActivityContract.Result) -> Unit = { result ->
         if (!result.errorMessage.isNullOrEmpty()) {
             onError(
@@ -51,33 +77,68 @@ internal fun GooglePayItem(
                 )
             }
     }
-
-    if (visibleCustomerFields.isEmpty()) {
-        GooglePayButton(
-            isEnabled = true,
-            onComplete = handle
-        )
-    } else {
-        ExpandablePaymentMethodItem(
-            method = method,
-            headerBackgroundColor = SDKTheme.colors.backgroundColor,
-            fallbackIcon = painterResource(id = SDKTheme.images.googlePayMethodResId),
-        ) {
-            Spacer(modifier = Modifier.size(10.dp))
-            CustomerFields(
-                visibleCustomerFields = visibleCustomerFields,
-                additionalFields = additionalFields,
-                customerFieldValues = method.customerFieldValues,
-                onCustomerFieldsChanged = { fields, isValid ->
-                    method.customerFieldValues = fields
-                    isCustomerFieldsValid = isValid
-                    method.isCustomerFieldsValid = isValid
-                }
+    val launcher = rememberLauncherForActivityResult(GooglePayActivityContract()) { result ->
+        isGooglePayOpened = false
+        handle(result)
+    }
+    val launchGooglePaySheet = {
+        isGooglePayOpened = true
+        launcher.launch(
+            GooglePayActivityContract.Config(
+                merchantId = merchantId,
+                merchantName = merchantName,
+                merchantEnvironment = paymentOptions.merchantEnvironment,
+                amount = paymentOptions.paymentInfo.paymentAmount,
+                currency = paymentOptions.paymentInfo.paymentCurrency
             )
-            Spacer(modifier = Modifier.size(22.dp))
+        )
+    }
+
+    if (!isForcePaymentMethod)
+        LaunchedEffect(Unit) {
+            val request =
+                IsReadyToPayRequest.fromJson(googlePayHelper.createReadyToPayRequest().toString())
+            googlePayClient.isReadyToPay(request).addOnCompleteListener { completedTask ->
+                isGooglePayAvailable = completedTask.isSuccessful
+            }
+        }
+
+    when {
+        customerFields.hasVisibleCustomerFields() -> {
+            ExpandablePaymentMethodItem(
+                method = method,
+                headerBackgroundColor = SDKTheme.colors.backgroundColor,
+                fallbackIcon = painterResource(id = SDKTheme.images.googlePayMethodResId),
+            ) {
+                Spacer(modifier = Modifier.size(10.dp))
+                CustomerFields(
+                    visibleCustomerFields = visibleCustomerFields,
+                    additionalFields = additionalFields,
+                    customerFieldValues = method.customerFieldValues,
+                    onCustomerFieldsChanged = { fields, isValid ->
+                        method.customerFieldValues = fields
+                        isCustomerFieldsValid = isValid
+                        method.isCustomerFieldsValid = isValid
+                    }
+                )
+                Spacer(modifier = Modifier.size(22.dp))
+                GooglePayButton(
+                    isEnabled = isCustomerFieldsValid && isGooglePayAvailable && !isGooglePayOpened,
+                    onClick = launchGooglePaySheet
+                )
+            }
+        }
+        customerFields.isAllCustomerFieldsHidden() && isForcePaymentMethod -> {
+            LaunchedEffect(key1 = Unit, block = { launchGooglePaySheet() })
             GooglePayButton(
-                isEnabled = isCustomerFieldsValid,
-                onComplete = handle
+                isEnabled = isGooglePayAvailable && !isGooglePayOpened,
+                onClick = launchGooglePaySheet
+            )
+        }
+        customerFields.isAllCustomerFieldsHidden() -> {
+            GooglePayButton(
+                isEnabled = isGooglePayAvailable && !isGooglePayOpened,
+                onClick = launchGooglePaySheet
             )
         }
     }
