66b55fd8a8e480f60724bf2b31139ac702b6b70e
Alexey Khrameev <a.khrameev@it.ecommpay.com>
2022-08-31 14:28:53 +0300

    moved example to another git repo

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -26,7 +26,7 @@ object Dependencies {
         const val junit4 = "androidx.compose.ui:ui-test-junit4:${Versions.compose}"
         const val navigation = "androidx.navigation:navigation-compose:2.4.2"
         const val lifecycleViewModel = "androidx.lifecycle:lifecycle-viewmodel-compose:2.4.1"
-        const val activity = "androidx.activity:activity-compose:1.4.0"
+        const val activity = "androidx.activity:activity-compose:1.5.1"
     }
 
     object Accompanist {
