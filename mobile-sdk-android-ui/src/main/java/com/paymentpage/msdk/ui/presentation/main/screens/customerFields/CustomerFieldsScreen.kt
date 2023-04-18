5286b8e139556115042a8d5038a78e012e85c774
Roman Karpilenko <r.karpilenko@it.ecommpay.com>
2023-04-18 15:19:14 +0300

    MSDK-724: Fixed appearing of payment id on the payment overview and description on the final screen

--- mobile-sdk-android-ui/src/main/java/com/paymentpage/msdk/ui/presentation/main/screens/customerFields/CustomerFieldsScreen.kt
+++ mobile-sdk-android-ui/src/main/java/com/paymentpage/msdk/ui/presentation/main/screens/customerFields/CustomerFieldsScreen.kt
@@ -4,11 +4,20 @@ import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
-import com.paymentpage.msdk.ui.*
+import com.paymentpage.msdk.ui.LocalMainViewModel
+import com.paymentpage.msdk.ui.LocalPaymentMethodsViewModel
+import com.paymentpage.msdk.ui.LocalPaymentOptions
+import com.paymentpage.msdk.ui.OverridesKeys
+import com.paymentpage.msdk.ui.SDKActionType
+import com.paymentpage.msdk.ui.TestTagsConstants
 import com.paymentpage.msdk.ui.presentation.main.fillCustomerFields
 import com.paymentpage.msdk.ui.presentation.main.sendCustomerFields
 import com.paymentpage.msdk.ui.theme.SDKTheme
@@ -36,7 +45,7 @@ internal fun CustomerFieldsScreen(
     SDKScaffold(
         title = getStringOverride(OverridesKeys.TITLE_PAYMENT_ADDITIONAL_DATA),
         scrollableContent = {
-            if (actionType == SDKActionType.Sale) {
+            if (actionType != SDKActionType.Tokenize) {
                 ExpandablePaymentOverview(
                     actionType = actionType,
                     expandable = true
