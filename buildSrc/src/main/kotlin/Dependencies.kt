15ad170e095785d2704bceb60f8e58b21d6de89c
Sergei Kamakov <sergei.kamakov@simbirsoft.com>
2025-07-28 11:28:38 +0300

    - Fix country dropdown menu (update msdk core 0.12.7) - Fix phone preselection - Implement options preselection (When additional field is set and field has selection options)

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -2,7 +2,7 @@
 
 object Library {
     const val artifactId = "msdk-ui"
-    const val version = "3.8.8"
+    const val version = "3.8.9"
     const val group = "com.ecommpay"
 }
 
@@ -10,7 +10,7 @@ object Versions {
     const val compose = "1.7.4"
     const val serialization = "1.3.3"
     const val detekt = "1.20.0"
-    const val msdkCore = "0.12.6"
+    const val msdkCore = "0.12.7"
 }
 
 object LibraryDependencies {
